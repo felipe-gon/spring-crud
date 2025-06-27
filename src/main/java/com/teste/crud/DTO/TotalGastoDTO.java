c073bee2727be6af35576449c300c39666c8d15b
felipe-gon <felipev2014@hotmail.com>
2025-06-26 22:34:13 -0300

    criando DTO para valor total gasto

---
+++ src/main/java/com/teste/crud/DTO/TotalGastoDTO.java
@@ -0,0 +1,13 @@
+package com.teste.crud.DTO;
+
+public class TotalGastoDTO {
+    private double valorTotalGasto;
+
+    public double getValorTotalGasto() {
+        return valorTotalGasto;
+    }
+
+    public void setValorTotalGasto(double valorTotalGasto) {
+        this.valorTotalGasto = valorTotalGasto;
+    }
+}
