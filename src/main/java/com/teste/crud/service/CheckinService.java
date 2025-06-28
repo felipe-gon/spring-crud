ac51c852526a36de43a8aeaa3b262337bbd4f306
felipe-gon <felipev2014@hotmail.com>
2025-06-28 20:05:25 -0300

    refatorando

--- src/main/java/com/teste/crud/service/CheckinService.java
+++ src/main/java/com/teste/crud/service/CheckinService.java
@@ -1,19 +1,12 @@
 package com.teste.crud.service;
 
-import com.teste.crud.DataFormatter;
 import com.teste.crud.model.CheckinModel;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.time.DayOfWeek;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 @Service
 public class CheckinService {
-
-    @Autowired
-    DataFormatter dataFormatter;
-
 
     public void diariaComGaragem (CheckinModel checkin){ //Cria o metodo para setar o valor da diaria recebendo o checkin como parâmetro
         double valorDiaria;
@@ -22,12 +15,15 @@ public class CheckinService {
 
         DayOfWeek diaSemana = checkin.getDataEntrada().getDayOfWeek();
         LocalTime horarioSaida = checkin.getDataSaida().toLocalTime();
+
         //Define que se a diária for em dia de semana (segunda a sexta), o valor será de R$120, e no final de semana R$150
-        if (diaSemana.getValue() >= DayOfWeek.MONDAY.getValue() && diaSemana.getValue() <= DayOfWeek.FRIDAY.getValue()) {
+        if (diaSemana.getValue() >= DayOfWeek.MONDAY.getValue() &&
+            diaSemana.getValue() <= DayOfWeek.FRIDAY.getValue() &&
+            checkin.isVeiculo()) {
             valorDiaria = 120;
-            if (checkin.isVeiculo())
-                valorGaragem = 15;
+            valorGaragem = 15;
         }
+        //Valores para finais de semana
         else {
             valorDiaria = 150;
             if (checkin.isVeiculo())
