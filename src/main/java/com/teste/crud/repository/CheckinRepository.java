d355d82e53b778dfe272f22526a590fcbffebab7
felipe-gon <felipev2014@hotmail.com>
2025-06-28 20:06:34 -0300

    adicionando método de busca

--- src/main/java/com/teste/crud/repository/CheckinRepository.java
+++ src/main/java/com/teste/crud/repository/CheckinRepository.java
@@ -1,10 +1,13 @@
 package com.teste.crud.repository;
 
 import com.teste.crud.model.CheckinModel;
+import com.teste.crud.model.UsuarioModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CheckinRepository extends JpaRepository<CheckinModel, Long> {
-
+List<CheckinModel> findByPessoa(UsuarioModel pessoa);
 }
