a0c681a0deb0fb764514b6ae063019cde3fd2d01
felipe-gon <felipev2014@hotmail.com>
2025-06-28 20:08:25 -0300

    refatorando

--- src/main/java/com/teste/crud/model/UsuarioModel.java
+++ src/main/java/com/teste/crud/model/UsuarioModel.java
@@ -1,50 +1,45 @@
 package com.teste.crud.model;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 import java.util.List;
 
-@Entity //Criar a tabela no BD
-@Table(name = "tb_usuario") //Cria a tabela com o nome tb_pessoa no banco
-@NoArgsConstructor //Cria o construtor vazio
-@AllArgsConstructor //Cria o construtor cheio
-@Data //Cria os getters e setters
+@Entity
+@Table(name = "tb_usuario")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 
 public class UsuarioModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
-
     @OneToMany(mappedBy = "pessoa")     //Cria o vinculo com o atributo pessoa da classe UsuarioModel, 1 Usuario para N checkin
+    @JsonIgnore
     private List<CheckinModel> checkins;
-
     private String documento;
     private String telefone;
+
 
     public void setNome(String nome) {
         this.nome = nome;
     }
-
     public String getDocumento() {
         return documento;
     }
-
     public void setDocumento(String documento) {
         this.documento = documento;
     }
-
     public String getTelefone() {
         return telefone;
     }
-
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
-
     public UsuarioModel(String nome){
         this.nome = nome;
     }
-
     public String getNome() {
         return nome;
     }
