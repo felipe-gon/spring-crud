2e5149156647ba18c63f122a9e605e2e6a5d8c27
felipe-gon <felipev2014@hotmail.com>
2025-06-28 20:10:27 -0300

    refatorando

--- src/main/java/com/teste/crud/model/CheckinModel.java
+++ src/main/java/com/teste/crud/model/CheckinModel.java
@@ -14,79 +14,64 @@ import java.time.LocalDateTime;
 
 public class CheckinModel {
 
+    //Getters e Setters
     public Long getId() {
         return id;
     }
-
+    public LocalDateTime getDataEntrada() {
+        return dataEntrada;
+    }
+    public void setDataEntrada(LocalDateTime dataEntrada) {
+        this.dataEntrada = dataEntrada;
+    }
+    public LocalDateTime getDataSaida() {
+        return dataSaida;
+    }
+    public void setDataSaida(LocalDateTime dataSaida) {
+        this.dataSaida = dataSaida;
+    }
+    public boolean isVeiculo() {
+        return veiculo;
+    }
+    public void setVeiculo(boolean veiculo) {
+        this.veiculo = veiculo;
+    }
+    public UsuarioModel getPessoa() {
+        return pessoa;
+    }
+    public void setPessoa(UsuarioModel pessoa) {
+        this.pessoa = pessoa;
+    }
+    public double getValorDiaria() {
+        return valorDiaria;
+    }
+    public void setValorDiaria(double valorDiaria) {
+        this.valorDiaria = valorDiaria;
+    }
+    public double getValorTotal() {
+        return valorTotal;
+    }
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+    public double getValorGaragem() {
+        return valorGaragem;
+    }
+    public void setValorGaragem(double valorGaragem) {
+        this.valorGaragem = valorGaragem;
+    }
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    //Getters e Setters
-    public LocalDateTime getDataEntrada() {
-        return dataEntrada;
-    }
-
-    public void setDataEntrada(LocalDateTime dataEntrada) {
-        this.dataEntrada = dataEntrada;
-    }
-
-    public LocalDateTime getDataSaida() {
-        return dataSaida;
-    }
-
-    public void setDataSaida(LocalDateTime dataSaida) {
-        this.dataSaida = dataSaida;
-    }
-
-    public boolean isVeiculo() {
-        return veiculo;
-    }
-
-    public void setVeiculo(boolean veiculo) {
-        this.veiculo = veiculo;
-    }
-
-    public UsuarioModel getPessoa() {
-        return pessoa;
-    }
-
-    public void setPessoa(UsuarioModel pessoa) {
-        this.pessoa = pessoa;
-    }
-
-    public double getValorDiaria() {
-        return valorDiaria;
-    }
-
-    public void setValorDiaria(double valorDiaria) {
-        this.valorDiaria = valorDiaria;
-    }
-
-    public double getValorTotal() {
-        return valorTotal;
-    }
-
-    public void setValorTotal(double valorTotal) {
-        this.valorTotal = valorTotal;
-    }
-
-    public double getValorGaragem() {
-        return valorGaragem;
-    }
-
-    public void setValorGaragem(double valorGaragem) {
-        this.valorGaragem = valorGaragem;
-    }
-
     @ManyToOne
     @JoinColumn(name = "usuario_id")
     private UsuarioModel pessoa;    //Atributo que vai vincular meu checkin ao meu usuario na hora de fazer o POST checkin
-
     @JsonFormat(pattern = "dd-MM-yyyy'T'HH:mm:ss")
     LocalDateTime dataEntrada;
     @JsonFormat(pattern = "dd-MM-yyyy'T'HH:mm:ss")
     LocalDateTime dataSaida;
+
     boolean veiculo;
     double valorDiaria;
     double valorTotal;
