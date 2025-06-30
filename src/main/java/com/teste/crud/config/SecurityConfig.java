4d496cd01d40e6b54bddb32d3326b57c9d6c29d9
felipe-gon <felipev2014@hotmail.com>
2025-06-30 20:47:36 -0300

    feat: criando config para autenticação oauth2

---
+++ src/main/java/com/teste/crud/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.teste.crud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/", "/login**", "/error**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2Login ->
+                        oauth2Login.defaultSuccessUrl("/", true)
+                );
+
+        return http.build();
+    }
+}
